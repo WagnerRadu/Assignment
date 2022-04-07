cb0b8ab02c6a038827b0df5a8eed23904a857353
HermanBob <raduwagner0@gmail.com>
2022-04-07 17:20:39 +0300

    User entity
    Signed-off-by: HermanBob <raduwagner0@gmail.com>

---
+++ src/main/java/com/example/demo/models/User.java
@@ -0,0 +1,34 @@
+package com.example.demo.models;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private int age;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+
+    public User(String firstName, String lastName, int age, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.password = password;
+    }
+}
