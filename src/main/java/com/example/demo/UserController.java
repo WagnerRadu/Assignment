4b69489a77d100e50dd6ea4f9aef1db64651fc25
HermanBob <raduwagner0@gmail.com>
2022-04-07 18:30:10 +0300

    getUserByFirstName
    Signed-off-by: HermanBob <raduwagner0@gmail.com>

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import com.example.demo.models.User;
 import com.example.demo.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,11 +21,15 @@ public class UserController {
 
     @PostMapping
     public ResponseEntity<?> createUser(@RequestBody User user) {
-        userRepository.save(user);
-        return new ResponseEntity<>(HttpStatus.OK);
+        try {
+            userRepository.save(user);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (DataAccessException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/find/byId/{id}")
     public ResponseEntity<?> getUserById(@PathVariable int id) {
         Optional<User> userOp = userRepository.findById(id);
         if(userOp.isPresent()) {
@@ -30,6 +37,17 @@ public class UserController {
         }
         else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/find/byName/{firstName}")
+    public ResponseEntity<?> getUserByFirstName(@PathVariable String firstName) {
+        List<User> userList = userRepository.findByFirstName(firstName);
+        if(userList.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        else {
+            return new ResponseEntity<>(userList, HttpStatus.OK);
         }
     }
 
--- src/main/java/com/example/demo/services/UserRepository.java
+++ src/main/java/com/example/demo/services/UserRepository.java
@@ -1,8 +1,14 @@
 package com.example.demo.services;
 
 import com.example.demo.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface UserRepository extends CrudRepository<User, Integer> {
-
+    @Query(value = "select * from users where first_name like %?1%", nativeQuery = true)
+    List<User> findByFirstName(String firstName);
 }
