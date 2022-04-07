c3de882c5473f8186d4e4c58e6fa32684055c585
HermanBob <raduwagner0@gmail.com>
2022-04-07 17:37:25 +0300

    create User
    Signed-off-by: HermanBob <raduwagner0@gmail.com>

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,7 +1,25 @@
 package com.example.demo;
 
+import com.example.demo.models.User;
+import com.example.demo.services.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<?> createUser(@RequestBody User user) {
+        userRepository.save(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
